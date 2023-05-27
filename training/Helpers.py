142cbfd8bd1868604d9c108dde756535387e57cd
Zajkos1 <mzx2000@gmail.com>
2023-05-27 17:01:13 +0200

    test_model

--- training/Helpers.py
+++ training/Helpers.py
@@ -1,20 +1,41 @@
 import pandas as pd
 import os
 
-
-def Preprocessing(sample_size = 10000,df=None):
-    
-    if df==None:
-        df = pd.read_csv('train.csv')
-        
-    sample = df.groupby('hotel_cluster').apply(lambda x: x.sample(frac=(sample_size/37670293)))
-    sample.drop(columns=['user_id'],inplace=True) 
+def Preprocessing(data):
+    data.drop(columns=['user_id'],inplace=True) 
     for column_name in ['date_time','srch_ci','srch_co']:
-        sample[column_name + '_month'] = pd.DatetimeIndex(sample[column_name]).month
-        sample[column_name +'_day'] = pd.DatetimeIndex(sample[column_name]).day
-        sample.drop(columns=[column_name],inplace=True)
-    sample.orig_destination_distance = sample.orig_destination_distance.fillna(sample.orig_destination_distance.mean())
+        data[column_name + '_month'] = pd.DatetimeIndex(data[column_name]).month
+        data[column_name +'_day'] = pd.DatetimeIndex(data[column_name]).day
+        data.drop(columns=[column_name],inplace=True)
+    data.orig_destination_distance = data.orig_destination_distance.fillna(data.orig_destination_distance.mean())
+    return data
+    
+
+def Take_data(sample_size = 10000,df=None):
+    #reading train data
+    if df==None:
+        df = pd.read_csv('../data/original_data/train.csv')
+        test_id = pd.read_csv('../data/datasets/test/test_id.csv')
+        df = df.loc[~df.index.isin(test_id)]
+    
+    #sampling or reading previosusly sampled data
+    if f'sample_{str(sample_size)}.csv' in os.listdir('../data/datasets/train'):
+        sample_id = pd.read_csv(os.path.join('../data/datasets/train'f'sample_{str(sample_size)}.csv'))
+        sample = df.loc[df.index.isin(sample_id)]
+    else:
+        sample = df.groupby('hotel_cluster').apply(lambda x: x.sample(frac=(sample_size/37670293)))
+    
+    #preprocessing    
+    sample = Preprocessing(data=sample)
     
     sample.index.to_csv(f'../data/datasets/train/sample_{str(sample_size)}.csv')
-    return
-    sample
+    
+    return sample
+
+def Test_model(model):
+    df = pd.read_csv('../data/original_data/train.csv')
+    test_id = pd.read_csv('../data/datasets/test/test_id.csv')
+    df = df.loc[df.index.isin(test_id)]
+    
+    model.predict()
+
